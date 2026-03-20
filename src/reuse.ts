faf6dbfce0bf0c2ba03959dd9f8e9745c82f31b1
turuslan <turuslan.devbox@gmail.com>
2026-03-20 18:38:27 +0300

    sigint sigterm

--- src/reuse.ts
+++ src/reuse.ts
@@ -25,3 +25,24 @@ export function hardlinkOverwrite(oldpath: string, newpath: string) {
   removePath(newpath, false);
   Deno.linkSync(oldpath, newpath);
 }
+
+export async function withSignal<T>(
+  fn: (abort: AbortController, signal: AbortSignal) => Promise<T>,
+) {
+  const abort = new AbortController();
+  function removeListenersAndAbort() {
+    for (const signal of signals) {
+      Deno.removeSignalListener(signal, removeListenersAndAbort);
+    }
+    abort.abort();
+  }
+  const signals: Deno.Signal[] = ["SIGINT", "SIGTERM"];
+  for (const signal of signals) {
+    Deno.addSignalListener(signal, removeListenersAndAbort);
+  }
+  try {
+    return await fn(abort, abort.signal);
+  } finally {
+    removeListenersAndAbort();
+  }
+}
