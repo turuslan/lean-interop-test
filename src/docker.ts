1711980adcd7205366fb17258167152cb74b4df0
turuslan <turuslan.devbox@gmail.com>
2026-03-21 07:28:50 +0300

    docker log

--- src/docker.ts
+++ src/docker.ts
@@ -2,6 +2,9 @@ import { dirname } from "jsr:@std/path/dirname";
 import { common } from "jsr:@std/path/common";
 import { join } from "jsr:@std/path/join";
 import { relative } from "jsr:@std/path/relative";
+import { logFile } from "./log.ts";
+import { Buffer } from "jsr:@std/io/buffer";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 export const ROOT_DIR_IN_DOCKER = "/lean-interop-test";
 export const ROOT_DIR = dirname(import.meta.dirname!);
@@ -24,12 +27,14 @@ async function http(
     json,
     expect_ok = true,
     expect_json = true,
+    expect_raw,
     signal,
   }: {
     query?: any;
     json?: any;
     expect_ok?: boolean;
     expect_json?: boolean;
+    expect_raw?: boolean;
     signal?: AbortSignal;
   } = {},
 ) {
@@ -54,6 +59,9 @@ async function http(
     console.warn(`${method} ${url.pathname}${url.search} = ${res.status}`);
     console.warn(await res.text());
     throw new Error();
+  }
+  if (expect_raw) {
+    return res;
   }
   if (expect_json) {
     return await res.json();
@@ -107,6 +115,75 @@ export async function docker_stop(name: string) {
   }
 }
 
+async function demux(
+  readable: ReadableStream<Uint8Array>,
+  log: (line: string) => void,
+) {
+  const buffer = new Buffer();
+  const stdout = new TransformStream();
+  const stdout_writer = stdout.writable.getWriter();
+  const stderr = new TransformStream();
+  const stderr_writer = stderr.writable.getWriter();
+  await Promise.all([
+    readable.pipeTo(
+      new WritableStream({
+        async write(chunk, controller) {
+          buffer.writeSync(chunk);
+          while (true) {
+            const buffered = buffer.bytes({ copy: false });
+            if (buffered.length < 8) break;
+            const stream_id = buffered[0];
+            if (stream_id < 1 || stream_id > 3) {
+              return controller.error(
+                new Error(`demux unexpected stream id ${stream_id}`),
+              );
+            }
+            const size = new DataView(
+              buffered.buffer,
+              buffered.byteOffset,
+              buffered.byteLength,
+            ).getUint32(4);
+            const row_size = 8 + size;
+            if (buffered.length < row_size) break;
+            await (stream_id === 1 ? stdout_writer : stderr_writer)
+              .write(buffered.slice(8, row_size));
+            buffer.readSync({
+              byteLength: row_size,
+              set(src: any, offset: any) {},
+            } as any);
+          }
+        },
+        async close() {
+          await Promise.all([stdout_writer.close(), stderr_writer.close()]);
+        },
+      }),
+    ),
+    ...[stdout, stderr].map((s) =>
+      s.readable
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(new TextLineStream())
+        .pipeTo(
+          new WritableStream({
+            write(line) {
+              log(line);
+            },
+          }),
+        )
+    ),
+  ]);
+}
+
+async function dockerLog(
+  name: string,
+  log: (line: string) => void,
+) {
+  const res: Response = await http("GET", `/containers/${name}/logs`, {
+    query: { follow: 1, stdout: 1, stderr: 1 },
+    expect_raw: true,
+  });
+  await demux(res.body!, log);
+}
+
 export async function docker_run(
   name: string,
   image: string,
@@ -117,6 +194,11 @@ export async function docker_run(
   if (CONTAINER_DIR === undefined) {
     throw new Error("CONTAINER_DIR env is missing");
   }
+  const log_path = join(ROOT_DIR, `data/${name}.log`);
+  Deno.mkdirSync(dirname(log_path), { recursive: true });
+  const log = logFile(log_path);
+  let logs_promise = Promise.resolve();
+  let exit_msg = "start error";
   try {
     await http("POST", "/containers/create", {
       query: { name },
@@ -131,14 +213,24 @@ export async function docker_run(
           Binds: [`${CONTAINER_DIR}:${ROOT_DIR_IN_DOCKER}`],
         },
       },
+      signal,
     });
-    await http("POST", `/containers/${name}/start`, { expect_json: false });
-    await http("POST", `/containers/${name}/wait`, {
+    log.log(`START ${JSON.stringify(cmd)}`);
+    await http("POST", `/containers/${name}/start`, {
+      expect_json: false,
+      signal,
+    });
+    logs_promise = dockerLog(name, log.log);
+    const status = await http("POST", `/containers/${name}/wait`, {
       query: { condition: "removed" },
       signal,
     });
+    exit_msg = JSON.stringify(status);
   } finally {
     await docker_stop(name);
+    await logs_promise;
+    log.log(`EXIT ${exit_msg}`);
+    log.close();
     signal.throwIfAborted();
   }
 }
