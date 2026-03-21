d01aae4cc7ab05219a46850c900949061feba048
turuslan <turuslan.devbox@gmail.com>
2026-03-21 14:13:12 +0300

    test, start, wait slot

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-const CONTAINER_NAME = Deno.env.get("CONTAINER_NAME");
-if (CONTAINER_NAME === undefined) {
-  console.info("CONTAINER_NAME env is missing");
-  Deno.exit();
+import * as ZEAM from "../clients/zeam.ts";
+import { runTest, Test } from "./test.ts";
+
+const slots = 10;
+
+async function test1(test: Test) {
+  test.start(test.clients);
+  for (let slot = 1; slot <= slots; ++slot) {
+    await test.waitSlot(slot, 0);
+    console.info(`slot ${slot}`);
+    await test.waitSlot(slot, 1);
+  }
 }
 
-console.info("TODO");
+await runTest(test1, {
+  clients: [ZEAM, ZEAM],
+  timeout_slots: slots + 1,
+});
---
+++ src/test.ts
@@ -0,0 +1,125 @@
+import { join } from "jsr:@std/path/join";
+import { delay } from "jsr:@std/async/delay";
+import {
+  docker_pull_many,
+  docker_run,
+  dockerName,
+  ROOT_DIR,
+} from "./docker.ts";
+import { withSignal } from "./reuse.ts";
+import { ClientArgs, genesis_generate } from "./genesis.ts";
+import { logFile } from "./log.ts";
+
+const SLOT_DURATION = 4000;
+const PHASES = 5;
+
+interface TestClient {
+  name: string;
+  process_promise: Promise<void> | null;
+  start(): void;
+}
+
+export class Test {
+  constructor(
+    public genesis_time: number,
+    public clients: TestClient[],
+    public signal: AbortSignal,
+  ) {}
+
+  start(clients: TestClient[]) {
+    for (const client of clients) {
+      client.start();
+    }
+  }
+
+  async waitSlot(slot: number, phase: number) {
+    if (phase < 0 || phase >= PHASES) throw new Error();
+    const now = Date.now();
+    const slot_time = this.genesis_time + slot * SLOT_DURATION;
+    const time = slot_time + phase * SLOT_DURATION / PHASES;
+    if (time <= now) {
+      return;
+    }
+    await delay(time - now, { signal: this.signal });
+  }
+}
+
+export interface TestClientArg {
+  NAME: string;
+  DOCKER_IMAGE: string;
+  dockerCmd(args: ClientArgs): string[];
+}
+export async function runTest(
+  test_fn: (test: Test) => Promise<void>,
+  args: { clients: TestClientArg[]; timeout_slots: number },
+) {
+  const root_dir = join(ROOT_DIR, "data");
+  const genesis_dir = join(root_dir, "genesis");
+  const names = args.clients.map((client, i) => `${client.NAME}_${i}`);
+  await withSignal(async (abort, signal) => {
+    await docker_pull_many(
+      args.clients.map((client) => client.DOCKER_IMAGE),
+      signal,
+    );
+    const genesis = await genesis_generate(genesis_dir, {
+      validators: names.length,
+      names,
+    }, signal);
+    const client_args = names.map<ClientArgs>((name, i) => ({
+      genesis_dir,
+      name,
+      data_dir: join(root_dir, name),
+      ports: genesis.ports[i],
+      node_key_path: genesis.nodeKeyPath(i),
+    }));
+    const logs = names.map((name) => logFile(join(root_dir, `${name}.log`)));
+    const timeout_timer = setTimeout(
+      () => abort.abort(),
+      (genesis.genesis_time - Date.now()) + args.timeout_slots * SLOT_DURATION,
+    );
+    const clients = args.clients.map<TestClient>((client, i) => ({
+      name: names[i],
+      process_promise: null,
+      start() {
+        const cmd = client.dockerCmd(client_args[i]);
+        if (genesis.isAggregator(i)) {
+          cmd.push("--is-aggregator");
+        }
+        if (this.process_promise !== null) {
+          throw new Error(`${name} already/still started`);
+        }
+        this.process_promise = docker_run(
+          dockerName(),
+          client.DOCKER_IMAGE,
+          cmd,
+          logs[i].log,
+          signal,
+        )
+          .finally(() => {
+            this.process_promise = null;
+          })
+          .catch(() => {
+            if (signal.aborted) return;
+            console.info(`${name} was not expected to stop`);
+            abort.abort();
+          });
+      },
+    }));
+    const test = new Test(genesis.genesis_time, clients, signal);
+    try {
+      await test_fn(test);
+      console.info("test ok");
+      abort.abort();
+    } finally {
+      clearTimeout(timeout_timer);
+      await Promise.all(
+        clients
+          .map((client) => client.process_promise)
+          .filter((promise) => promise !== null),
+      );
+      for (const log of logs) {
+        log.close();
+      }
+    }
+  });
+}
